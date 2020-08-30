716769f642568ebb5f03c731f29d104b774abc09
marco sena <marcoacsena@gmail.com>
2020-08-30 18:47:15 -0300

    Abrir e fechar Fragment dinamicamente.

---
+++ app/src/main/java/com/example/tiafragmentexample2/SimpleFragment.java
@@ -0,0 +1,118 @@
+package com.example.tiafragmentexample2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+    private static final int YES = 0;
+    private static final int NO = 1;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SimpleFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance() {
+        //Retirei esses parâmetros (String param1, String param2) do método!
+//        SimpleFragment fragment = new SimpleFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+        return new SimpleFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        final RatingBar ratingBar = rootView.findViewById(R.id.ratingBarSong);
+
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = rootView.findViewById(R.id.fragment_header);
+
+                switch (index) {
+
+                    case 0: //User chose YES
+                        textView.setText(R.string.yes_message);
+                        break;
+
+                    case NO: // User chose "No."
+                        textView.setText(R.string.no_message);
+                        break;
+                    default: // No choice made.
+                        // Do nothing.
+                        break;
+                }
+            }
+        });
+
+        // Set the rating bar onCheckedChanged listener.
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+                // Get rating and show Toast with rating.
+
+                String myRating = (getString(R.string.my_rating) + ": "
+                        + String.valueOf(ratingBar.getRating()));
+                Toast.makeText(getContext(), myRating,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return rootView;
+
+    }
+
+}
